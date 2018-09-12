701791e0c01b15f55e3fa131aafeecf86fdfae74
Andreas Hansson <andreas.hansson@icomera.com>
2018-09-12 09:13:37 +0200

    Builded react app added for test hopefully it works

--- server/app.py
+++ server/app.py
@@ -1,10 +1,10 @@
 import flask
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, static_folder="build/static", template_folder="build/")
 
 @app.route("/")
 def index():
-    return flask.jsonify({"test": "test"})
+    return flask.render_template('index.html')
 
 
 if __name__ == '__main__':
